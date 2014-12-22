f8ea4532105e97e6bbd7ed1171877f7865071f85
Martina Maggio <maggio.martina@gmail.com>
2014-12-22 19:06:58 +0800

    Substituting ifconfig with ip addr, the first is deprecated

--- launch.sh
+++ launch.sh
@@ -19,7 +19,7 @@ REMOTE_username=$3
 REFERENCE_run=$4
 REFERENCE_trace=$5
 LISTENING_PORT="23958"
-LISTENER_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+LISTENER_IP=$(ip addr | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 TRACE_CMD_COMMAND="trace-cmd"
 
 # --------------------------------------------------------------------
